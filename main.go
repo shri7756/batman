a94101589b467e45384b705e51a822126fe7f05f
sxh8175 <shri.hallikhed@gmail.com>
2021-01-08 13:34:36 -0500

    updated

--- main.go
+++ main.go
@@ -1,32 +1,8 @@
-// [START container_batman]
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-	"os"
+        "fmt"
 )
-
 func main() {
-	// register batman function to handle all requests
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", batman)
-
-	// use PORT environment variable, or default to 8080
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
-
-	// start the web server on port and accept requests
-	log.Printf("Server listening on port %s", port)
+ fmt.printf("A wealthy American playboy")
 }
-
-// batman responds to the request with a plain-text.
-func batman(w http.ResponseWriter, r *http.Request) {
-	log.Printf("Serving request: %s", r.URL.Path)
-	fmt.Fprintf(w, "A wealthy American playboy\n")
-}
-
-// [END container_batman]
